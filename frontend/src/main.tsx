e404139b14a0ddeb33ff467a8e50b875d824cf7f
Yitong Wu <wuyt26@outlook.com>
2024-03-12 21:13:03 -0700

    ip debugging

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -7,7 +7,7 @@ import './index.css'
 
 
 const client = new ApolloClient({
-  uri: 'http://127.0.0.1:6868/api/graphql',
+  uri: 'http://54.193.206.20:6868/api/graphql',
   cache: new InMemoryCache()
 });
 
